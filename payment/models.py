87358ddcca2a3c61700ced43b9f951c5f93eceed
NagnathSomanwar <nagnathsomanwar@gmail.com>
2024-08-15 15:25:46 +0530

    bulid Customer Order Models

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 
 # Create your models here.
@@ -21,3 +22,29 @@ class ShippingAddress(models.Model):
 
         def __str__(self):
             return f'Shipping Address -{str(self.id)}'
+
+
+# Created Order Model
+class Order(models.Model):
+    # Foregin key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
+    data_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order- {str(self.id)}'
+
+
+# create Order items models
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'order items-{str(self.id)}'
--- store/views.py
+++ store/views.py
@@ -65,8 +65,8 @@ def login_user(request):
                 # Get the cart
                 cart = Cart(request)
                 # let's loop thru the dictionary and add the items  from the database
-                for key,value in converted_cart.items():
-                    cart.db_add(product=key,quantity=value)
+                for key, value in converted_cart.items():
+                    cart.db_add(product=key, quantity=value)
             messages.success(request, "You Have Been Logged In")
             return redirect("home")
         else:
@@ -148,11 +148,11 @@ def update_info(request):
     if request.user.is_authenticated:
         current_user = Profile.objects.get(user__id=request.user.id)
         # Get current  User's shipping address
-        shipping_user =ShippingAddress.objects.get(id=request.user.id)
+        shipping_user = ShippingAddress.objects.get(id=request.user.id)
         # Get original User  Form
         form = UserInfoForm(request.POST or None, instance=current_user)
         # Get  user's  Shipping Form
-        shipping_form =ShippingForm(request.POST or None,instance=shipping_user)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
         if form.is_valid() or shipping_form.is_valid():
             # save the Original form
             form.save()
@@ -160,7 +160,7 @@ def update_info(request):
             shipping_form.save()
             messages.success(request, 'Your Info Has Been Updated!!')
             return redirect('home')
-        return render(request, 'update_info.html', {'form': form ,'shipping_form':shipping_form})
+        return render(request, 'update_info.html', {'form': form, 'shipping_form': shipping_form})
     else:
         messages.success(request, 'You Must Be logged In')
         return render('home')
