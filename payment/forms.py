e73424f5519df3524dcfdcc2b33d54d50005fe60
NagnathSomanwar <nagnathsomanwar@gmail.com>
2024-08-20 22:13:00 +0530

    added billing info

--- payment/forms.py
+++ payment/forms.py
@@ -3,28 +3,28 @@ from .models import ShippingAddress
 
 
 class ShippingForm(forms.ModelForm):
-    shipping_full_name = forms.CharField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Full Name'}), required=True)
-    shipping_email = forms.CharField(label='',
-                                     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-                                     required=True)
-    shipping_address1 = forms.CharField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Bulding Floor Door No'}), required=True)
-    shipping_address2 = forms.CharField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Address'}), required=True)
-    shipping_city = forms.CharField(label='',
-                                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),
-                                    required=True)
-    shipping_state = forms.CharField(label='',
-                                     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State'}),
-                                     required=False)
-    shipping_pincode = forms.CharField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'PinCode'}), required=False)
-    shipping_country = forms.CharField(label='', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Country'}), required=True)
+    shipping_full_name = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Full Name'}), required=True)
+    shipping_email = forms.CharField(label='',widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),required=True)
+    shipping_address1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Bulding Floor Door No'}), required=True)
+    shipping_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}), required=True)
+    shipping_city = forms.CharField(label='',widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'City'}),required=True)
+    shipping_state = forms.CharField(label='',widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'State'}),required=False)
+    shipping_pincode = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'PinCode'}), required=False)
+    shipping_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Country'}), required=True)
 
     class Meta:
         model = ShippingAddress
         # fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_city',
         #           'shipping_state', 'shipping_pincode', 'shipping_country']
         exclude=['user',]
+
+class  PaymentForm(forms.Form):
+    card_name = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Name on card'}), required=True)
+    card_number = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Card Number'}), required=True)
+    card_exp_data = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Expire date'}), required=True)
+    card_cvv_number = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'cvv code'}), required=True)
+    card_address1 = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Billing Address1'}), required=True)
+    card_address2 = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Billing Address2'}), required=False)
+    card_city = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'billing City'}), required=True)
+    card_pincode = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'pincode'}), required=True)
+    card_state = forms.CharField(label='', widget=forms.TextInput( attrs={'class': 'form-control', 'placeholder': 'Billing State'}), required=True)
--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from .forms import ShippingForm
+from .forms import ShippingForm, PaymentForm
 from .models import ShippingAddress
+from django.contrib import messages
 
 
 # Create your views here.
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods()
+        quantities = cart.get_quants()
+        total = cart.cart_total()
+        if request.user.is_authenticated:
+            # GEt the billing form
+            billing_form = PaymentForm()
+            return render(request, 'billing_info.html',{'cart_products': cart_products, 'quantities': quantities, 'total': total,'shipping_info': request.POST,'billing_form':billing_form})
+
+        else:
+            return render(request, 'billing_info.html',{'cart_products': cart_products, 'quantities': quantities, 'total': total,'shipping_info': request.POST})
+        shipping_form=request.POST
+        return render(request, 'billing_info.html',{'cart_products': cart_products, 'quantities': quantities, 'total': total,'shipping_info':shipping_form })
+    else:
+        messages.error(request, "Access Tonight")
+        return redirect('home')
+
+
 def checkout(request):
     # Get the cart
     cart = Cart(request)
@@ -20,11 +41,8 @@ def checkout(request):
                       {'cart_products': cart_products, 'quantities': quantities, 'total': total,
                        'shipping_form': shipping_form})
     shipping_form = ShippingForm()
-    return render(request, 'checkout.html',
-                  {'cart_products': cart_products, 'quantities': quantities, 'total': total,
-                   'shipping_form': shipping_form})
+    return render(request, 'checkout.html',{'cart_products': cart_products, 'quantities': quantities, 'total': total,'shipping_form': shipping_form})
 
 
 def payment_success(request):
     return render(request, 'payment_success.html', {})
-
